522e6aaef91dfe182acda3adc39c78961d952bfd
Balint Kovari <balintmaci@gmail.com>
2021-11-06 15:29:18 +0100

    test second example

--- dataquery/queryapi/tests.py
+++ dataquery/queryapi/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 import types
-import re
 from pathlib import Path
 from .apis import *
 
@@ -26,4 +25,12 @@ class SearchTestCase(TestCase):
 
 
     def test_second_example_query(self):
-        pass
+        params = { 'attribute' : 'priority', 'value' : 'high' }
+        request = types.SimpleNamespace()
+        request.GET = types.SimpleNamespace()
+        request.GET.get = lambda key, default: params.get(key) if params.get(key) else default
+        result = search_request(request)
+        BASE_DIR = Path(__file__).resolve().parent
+        TEMPLATE_PATH = Path.joinpath(BASE_DIR, "static", "example2.txt")
+        with open(TEMPLATE_PATH, 'r') as f:
+            self.assertEqual(result.content.decode('utf-8'), json.dumps(json.loads(f.read())))
